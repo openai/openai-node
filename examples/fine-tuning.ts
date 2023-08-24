6fea1a54a6921a1b3ca32e2acf773992aabdc98d
simonpfish <simonpfish@gmail.com>
2023-08-23 17:16:15 -0700

    update data and event listening logic

--- examples/fine-tuning.ts
+++ examples/fine-tuning.ts
@@ -10,6 +10,7 @@
 
 import fs from 'fs';
 import OpenAI from 'openai';
+import { FineTuningJobEvent } from 'openai/resources/fine-tuning';
 
 // Gets the API Key from the environment variable `OPENAI_API_KEY`
 const client = new OpenAI();
@@ -18,13 +19,13 @@ async function main() {
   console.log(`Uploading file`);
 
   let file = await client.files.create({
-    file: fs.createReadStream('./examples/fine-tune-data.jsonl'),
+    file: fs.createReadStream('./examples/fine-tuning-data.jsonl'),
     purpose: 'fine-tune',
   });
   console.log(`Uploaded file with ID: ${file.id}`);
+
   console.log('-----');
 
-  // Wait for the file to be processed
   console.log(`Waiting for file to be processed`);
   while (true) {
     file = await client.files.retrieve(file.id);
@@ -42,23 +43,23 @@ async function main() {
   console.log(`Starting fine-tuning`);
   let fineTune = await client.fineTuning.jobs.create({ model: 'gpt-3.5-turbo', training_file: file.id });
   console.log(`Fine-tuning ID: ${fineTune.id}`);
+
   console.log('-----');
 
   console.log(`Track fine-tuning progress:`);
-  let after: string | undefined;
-  while (fineTune.status !== 'succeeded') {
+
+  const events: Record<string, FineTuningJobEvent> = {};
+
+  while (fineTune.status == 'running' || fineTune.status == 'created') {
     fineTune = await client.fineTuning.jobs.retrieve(fineTune.id);
     console.log(`${fineTune.status}`);
 
-    const options = after ? { limit: 50, after } : { limit: 50 };
-    const events = await client.fineTuning.jobs.listEvents(fineTune.id, options);
-    for (const event of events.data.reverse()) {
-      console.log(`- ${event.created_at}: ${event.message}`);
-    }
-
-    if (events.data.length > 0) {
-      after = events.data[events.data.length - 1]?.id;
-      console.log(after);
+    const { data } = await client.fineTuning.jobs.listEvents(fineTune.id, { limit: 100 });
+    for (const event of data.reverse()) {
+      if (event.id in events) continue;
+      events[event.id] = event;
+      const timestamp = new Date(event.created_at * 1000);
+      console.log(`- ${timestamp.toLocaleTimeString()}: ${event.message}`);
     }
 
     await new Promise((resolve) => setTimeout(resolve, 5000));
