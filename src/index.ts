5236bbc16fe4f2f93ee3398299c1d875e60fde64
Deyaaeldeen Almahallawi <dealmaha@microsoft.com>
2024-08-20 15:46:03 -0700

    [Azure] Refresh AAD token on retry (#1003)
    * [Azure] Refresh AAD token on retry
    
    * add context

--- src/index.ts
+++ src/index.ts
@@ -485,7 +485,13 @@ export class AzureOpenAI extends OpenAI {
   }
 
   protected override async prepareOptions(opts: Core.FinalRequestOptions<unknown>): Promise<void> {
-    if (opts.headers?.['Authorization'] || opts.headers?.['api-key']) {
+    /**
+     * The user should provide a bearer token provider if they want
+     * to use Azure AD authentication. The user shouldn't set the
+     * Authorization header manually because the header is overwritten
+     * with the Azure AD token if a bearer token provider is provided.
+     */
+    if (opts.headers?.['api-key']) {
       return super.prepareOptions(opts);
     }
     const token = await this._getAzureADToken();
