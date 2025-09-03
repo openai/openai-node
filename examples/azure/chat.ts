350f1b3262e62904373d8d805df50cf8005c27de
Deyaa <dealmaha@microsoft.com>
2025-09-03 20:31:42 +0000

    [azure] Edit examples

--- examples/azure/chat.ts
+++ examples/azure/chat.ts
@@ -1,20 +1,18 @@
 #!/usr/bin/env -S npm run tsn -T
 
-import { AzureOpenAI } from 'openai';
+import { OpenAI } from 'openai';
 import { getBearerTokenProvider, DefaultAzureCredential } from '@azure/identity';
 import 'dotenv/config';
 
 // Corresponds to your Model deployment within your OpenAI resource, e.g. gpt-4-1106-preview
 // Navigate to the Azure OpenAI Studio to deploy a model.
-const deployment = 'gpt-4-1106-preview';
-
+const deployment = 'gpt-5';
+const baseURL = process.env?.['AZURE_OPENAI_ENDPOINT'];
 const credential = new DefaultAzureCredential();
 const scope = 'https://cognitiveservices.azure.com/.default';
-const azureADTokenProvider = getBearerTokenProvider(credential, scope);
+const apiKey = getBearerTokenProvider(credential, scope);
 
-// Make sure to set AZURE_OPENAI_ENDPOINT with the endpoint of your Azure resource.
-// You can find it in the Azure Portal.
-const openai = new AzureOpenAI({ azureADTokenProvider, apiVersion: '2024-10-01-preview' });
+const openai = new OpenAI({ baseURL: baseURL + '/openai/v1', apiKey });
 
 async function main() {
   console.log('Non-streaming:');
