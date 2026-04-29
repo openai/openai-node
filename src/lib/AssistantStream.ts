e67769de2ff98e1ac43bb108463b52bcdfcbbe11
Genmin <joey@joeyroth.com>
2026-04-29 16:11:40 -0700

    fix helper method header override

--- src/lib/AssistantStream.ts
+++ src/lib/AssistantStream.ts
@@ -196,7 +196,7 @@ export class AssistantStream
     runner._run(() =>
       runner._runToolAssistantStream(runId, runs, params, {
         ...options,
-        headers: { ...options?.headers, 'X-Stainless-Helper-Method': 'stream' },
+        __metadata: { ...options?.__metadata, helperMethod: 'stream' },
       }),
     );
     return runner;
@@ -241,7 +241,7 @@ export class AssistantStream
     runner._run(() =>
       runner._threadAssistantStream(params, thread, {
         ...options,
-        headers: { ...options?.headers, 'X-Stainless-Helper-Method': 'stream' },
+        __metadata: { ...options?.__metadata, helperMethod: 'stream' },
       }),
     );
     return runner;
@@ -257,7 +257,7 @@ export class AssistantStream
     runner._run(() =>
       runner._runAssistantStream(threadId, runs, params, {
         ...options,
-        headers: { ...options?.headers, 'X-Stainless-Helper-Method': 'stream' },
+        __metadata: { ...options?.__metadata, helperMethod: 'stream' },
       }),
     );
     return runner;
