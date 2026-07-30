b1fc7df4659aaf2cac0c78d3f5b6814251de2b0b
Hayden <hayden@openai.com>
2026-07-30 15:04:55 -0700

    chore: remove JSR publishing (#2037)
    * chore: remove JSR publishing
    
    * [ecosystem-tests] validate Deno npm artifact
    
    * [ecosystem-tests] fix Deno npm installs and cleanup
    
    * [uploads] consume FormData capability probe responses

--- ecosystem-tests/deno/main_test.ts
+++ ecosystem-tests/deno/main_test.ts
@@ -1,6 +1,6 @@
 import { assertEquals, AssertionError } from 'https://deno.land/std@0.192.0/testing/asserts.ts';
 import { distance } from 'https://deno.land/x/fastest_levenshtein/mod.ts';
-import OpenAI, { toFile } from 'openai';
+import OpenAI, { toFile } from 'npm:openai';
 
 const url = 'https://audio-samples.github.io/samples/mp3/blizzard_biased/sample-1.mp3';
 const filename = 'sample-1.mp3';
