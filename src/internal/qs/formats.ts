46c17f4f828ab6c35868c7d42021e4c18197ceac
Hayden <hayden@openai.com>
2026-08-05 18:59:24 +0000

    chore(lint): enable unicorn/prefer-native-coercion-functions

--- src/internal/qs/formats.ts
+++ src/internal/qs/formats.ts
@@ -1,7 +1,7 @@
 import type { Format } from './types';
 
 export const default_format: Format = 'RFC3986';
-export const default_formatter = (v: PropertyKey) => String(v);
+export const default_formatter: (value: PropertyKey) => string = String;
 export const formatters: Record<Format, (str: PropertyKey) => string> = {
   RFC1738: (v: PropertyKey) => String(v).replace(/%20/g, '+'),
   RFC3986: default_formatter,
