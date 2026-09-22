7e3ecf424cdd85611215a451e1b04989fe5ae0f1
Castiron <noreply@openai.com>
2026-09-22 11:00:48 -0700

    Castiron generated snapshot
    Castiron-Public-Codegen-Version: 1
    Castiron-Public-Codegen-Generation-ID: 81228844-6bd5-4351-8a5b-dc0f509e7769
    Castiron-Public-Codegen-Hash: 45008f0461bbda2208e069a9a9f89de2f3873ff208f4133b9d599d5d4ec5313c
    Castiron-Public-Codegen-Branch: codegen/castiron/promotions/pr-136

--- src/resources/beta/responses/responses.ts
+++ src/resources/beta/responses/responses.ts
@@ -1419,6 +1419,8 @@ export interface BetaResponse {
    */
   model:
     | 'gpt-6-astra'
+    | 'gpt-6-sol'
+    | 'gpt-6-luna'
     | 'gpt-5.6-sol'
     | 'gpt-5.6-terra'
     | 'gpt-5.6-luna'
@@ -11998,6 +12000,8 @@ export namespace BetaResponsesClientEvent {
      */
     model?:
       | 'gpt-6-astra'
+      | 'gpt-6-sol'
+      | 'gpt-6-luna'
       | 'gpt-5.6-sol'
       | 'gpt-5.6-terra'
       | 'gpt-5.6-luna'
@@ -14240,6 +14244,8 @@ export interface ResponseCreateParamsBase {
    */
   model?:
     | 'gpt-6-astra'
+    | 'gpt-6-sol'
+    | 'gpt-6-luna'
     | 'gpt-5.6-sol'
     | 'gpt-5.6-terra'
     | 'gpt-5.6-luna'
@@ -14910,6 +14916,8 @@ export interface ResponseCompactParams {
    */
   model:
     | 'gpt-6-astra'
+    | 'gpt-6-sol'
+    | 'gpt-6-luna'
     | 'gpt-5.6-sol'
     | 'gpt-5.6-terra'
     | 'gpt-5.6-luna'
